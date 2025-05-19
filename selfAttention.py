9a57887f7213f400f248cfbf739ecd6fce4aa0bf
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2025-05-19 12:45:46 +0200

    computing manually in an expensive way the self attention mechanism to understand it better

--- selfAttention.py
+++ selfAttention.py
@@ -1,4 +1,5 @@
 import torch
+import math
 sentence = torch.tensor(
     [0, # can
      7, # you
@@ -30,3 +31,46 @@ omega_mat = embedded_sentence.matmul(embedded_sentence.T)
 # since the previous is a repetition, the following simply check if the result is the same
 torch.allclose(omega_mat, omega)
 print(torch.allclose(omega_mat, omega))
+
+# computing the attention weight using pytorch's softmax function
+import torch.nn.functional as F
+attention_weights = F.softmax(omega, dim=1)
+attention_weights.shape
+print(attention_weights.shape)
+# print(attention_weights)
+print(attention_weights.sum(dim=1))
+
+x_2 = embedded_sentence[1, :]
+context_vec_2 = torch.zeros(x_2.shape)
+for j in range(8):
+    x_j = embedded_sentence[j, :]
+    context_vec_2 += attention_weights[1, j] * x_j
+print(f'original matrix of attention weights : {context_vec_2}')
+# try with raw math to get this straight
+# from the embedded sentence getting the scalars for the new matrix calculating the dot product between the two vectors
+for i in range(len(embedded_sentence)):
+    attention_weightss = []
+    for j in range(len(embedded_sentence)): 
+        a = 0
+        for index in range(len(embedded_sentence[j])):
+            a += embedded_sentence[j][index] * embedded_sentence[i][index]
+        attention_weightss.append(a)
+    sum_of_exponential = 0
+    s_m_values = []
+    for exp in range(len(attention_weightss)):
+        sum_of_exponential = sum_of_exponential + math.exp(attention_weightss[exp]) # math.exp is the equivalent to the concept e^exp
+    for index_att in range(len(attention_weightss)):
+        single_soft_valu = math.exp(attention_weightss[index_att]) / sum_of_exponential
+        s_m_values.append(single_soft_valu)
+    zi_vectors_prev = []
+
+    att_for_all_weight = [0] * len(embedded_sentence[i]) # initializing all weight at 0
+    for indAttW in range(len(s_m_values)):
+        att_for_spec_weight = []
+        for indInsWord in range(len(embedded_sentence[i])):
+            att_for_spec_weight.append(s_m_values[indAttW] * embedded_sentence[i][indInsWord])
+        for at_ind_sp in range(len(att_for_spec_weight)):
+            att_for_all_weight[at_ind_sp] += att_for_spec_weight[at_ind_sp]
+
+    print(f'Manual att for all weight: {att_for_all_weight}')
+        
