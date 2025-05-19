8015d6156db190d0280afe13c14779dbc8d61440
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2025-05-19 10:53:13 +0200

    computing the weight

--- selfAttention.py
+++ selfAttention.py
@@ -9,3 +9,24 @@ sentence = torch.tensor(
      4, # this
      3] # sentence
     )
+
+print(f'sentence: {sentence}')
+
+# the following code will produce word embedding of our eight words
+torch.manual_seed(123)
+embed = torch.nn.Embedding(10, 16)
+embedded_sentence = embed(sentence).detach()
+embedded_sentence.shape
+print(embedded_sentence.shape)
+# print(embedded_sentence)
+# Now we compute the dot product
+omega = torch.empty(8,8)
+for i, x_i in enumerate(embedded_sentence):
+    for j, x_j in enumerate(embedded_sentence):
+        omega[i, j] = torch.dot(x_i, x_j)
+
+omega_mat = embedded_sentence.matmul(embedded_sentence.T)
+
+# since the previous is a repetition, the following simply check if the result is the same
+torch.allclose(omega_mat, omega)
+print(torch.allclose(omega_mat, omega))
